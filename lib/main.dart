3bbf78b3172ae8b696a9e98aa15b88a3a5f05ecf
vignesh-sniper <vignesh.sniper@gmail.com>
2023-07-25 23:37:22 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       title: 'VLAD - Anime Downloader',
       theme: ThemeData(
         primarySwatch: Colors.blue,
